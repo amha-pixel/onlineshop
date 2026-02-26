7eefeefc10de31bcba933a0ee01d9026335082d0
Amalie Hammel <hammelamalie@outlook.dk>
2026-02-26 14:02:42 +0100

    add dynamisk forside med URL-parametre

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@ fetch("https://kea-alt-del.dk/t7/api/seasons")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((seasons) => {
-      categoryContainer.innerHTML += `<a href="produktliste.html" class="kategori-card" data-field="seasons">${seasons.season}</a>`;
+      categoryContainer.innerHTML += `<a href="produktliste.html?season=${seasons.season}" class="kategori-card" data-field="seasons">${seasons.season}</a>`;
     });
   });
